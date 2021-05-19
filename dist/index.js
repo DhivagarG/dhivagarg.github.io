52b7025ec6a523bd5f793fa1d35f09ba6e994782
Dhivagar <dhivagar.gopu@accenture.com>
2021-05-19 17:39:28 +0530

    latest

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,13 @@
 (function () {
-  // document.getElementById('items').addEventListener('click', (e) => {
-  //   const item = e.target.innerText;
-  //   const fileName = `${item.toLowerCase()}.css`;
-  //   const fileRef = document.createElement('link');
-  //   fileRef.setAttribute('rel', 'stylesheet');
-  //   fileRef.setAttribute('type', 'text/css');
-  //   fileRef.setAttribute('href', fileName);
-  //   document.getElementsByTagName('head')[0].appendChild(fileRef);
-  // });
+  document.getElementById('hamburger-menu').addEventListener('click', (e) => {
+    const item = e.target.dataset.superhero;
+    const fileName = `styles/${item.toLowerCase()}.css`;
+    const fileRef = document.createElement('link');
+    fileRef.setAttribute('rel', 'stylesheet');
+    fileRef.setAttribute('type', 'text/css');
+    fileRef.setAttribute('href', fileName);
+    document.getElementsByTagName('head')[0].appendChild(fileRef);
+  });
   document.getElementById('hamburger-icon').addEventListener('click', () => {
     const menu = document.getElementById('hamburger-menu');
     menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';

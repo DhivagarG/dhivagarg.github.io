2c1eadd5fd8b744e415e3e5063319dd7b2636cc3
3fellasinc <3fellasinc@gmail.com>
2022-02-07 16:54:53 +0530

    Updates

---
+++ home.js
@@ -0,0 +1,130 @@
+const home  = `<header>
+<div>
+    <img alt="theme-menu" src="assets/superhero-svgrepo-com.svg" id="hamburger-icon" />
+    <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+        id="hamburger-icon" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+        stroke-linejoin="round" class="feather feather-align-justify">
+        <line x1="21" y1="10" x2="3" y2="10"></line>
+        <line x1="21" y1="6" x2="3" y2="6"></line>
+        <line x1="21" y1="14" x2="3" y2="14"></line>
+        <line x1="21" y1="18" x2="3" y2="18"></line>
+    </svg> -->
+</div>
+<div id="hamburger-menu">
+    <div data-superhero="batman" onclick="applyTheme('batman');"></div>
+    <div data-superhero="superman" onclick="applyTheme('superman');"></div>
+    <div data-superhero="spiderman" onclick="applyTheme('spiderman');"></div>
+    <div data-superhero="captainamerica" onclick="applyTheme('captainamerica');"></div>
+</div>
+</header>
+<section>
+<img src="./assets/dg.png" alt="profile" />
+<div class="content" id="about">
+    <h1>
+        Hi There, I am
+    </h1>
+    <div>
+        <img src="./assets/dhivagar.gif" class="gif" />
+    </div>
+    <h3>
+        I am a front-end developer having experience with developing responsive web applications using HTML,
+        CSS, JavaScript and libaries/frameworks like React.js and Angular.
+    </h3>
+    <h6>
+        Here's how you can reach me
+    </h6>
+</div>
+<div id="contact">
+    <div class="icons">
+        <a href="mailto:dhivadhinasachin@gmail.com" title="Email: dhivadhinasachin@gmail.com"
+            target="_blank" rel="noreferrer">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="feather feather-mail">
+                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z">
+                </path>
+                <polyline points="22,6 12,13 2,6"></polyline>
+            </svg>
+        </a>
+        <a href="https://www.linkedin.com/in/dhivagar-gopu-bab627a3/" target="_blank" rel="noreferrer"
+            title="Linkedin">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="feather feather-linkedin">
+                <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z">
+                </path>
+                <rect x="2" y="9" width="4" height="12"></rect>
+                <circle cx="4" cy="4" r="2"></circle>
+            </svg>
+        </a>
+        <a href="https://github.com/DhivagarG" target="_blank" rel="noreferrer" title="Github">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="feather feather-github">
+                <path
+                    d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
+                </path>
+            </svg>
+        </a>
+    </div>
+</div>
+<div id="projects">
+    <h3>Projects</h3>
+    <li> <a href="#" onclick="onNavigate('/bodypix'); return false;">TensorFlow Body Pix</a></li>
+
+</div>
+</section>`;
+
+//(function () {
+    //if(document.getElementById('hamburger-menu')){
+    //document.getElementById('hamburger-menu').addEventListener('click', (e) => {
+    const applyTheme = (item) => {
+      //const item = e.target.dataset.superhero;
+      if (item) {
+        const fileName = `styles/${item?.toLowerCase()}.css`;
+        const fileRef = document.createElement('link');
+        // Get existing stylesheet
+        const hero = document.getElementById('hero');
+        fileRef.setAttribute('rel', 'stylesheet');
+        fileRef.setAttribute('type', 'text/css');
+        fileRef.setAttribute('href', fileName);
+        fileRef.setAttribute('id', 'hero');
+        if (hero) {
+          // Remove unused stylesheet
+          hero.remove();
+        }
+        document.getElementsByTagName('head')[0].appendChild(fileRef);
+        for (let superhero of document.querySelectorAll('[data-superhero]')) {
+          if (item === superhero.getAttribute('data-superhero')) {
+            superhero.classList.add('active-theme');
+          } else {
+            superhero.classList.remove('active-theme');
+          }
+        }
+      }
+    }
+    //});
+//}
+    const toggleHamburgerMenu = (arg) => {
+      const menu = document.getElementById('hamburger-menu');
+      if (arg) {
+        menu.style.display = arg;
+      } else {
+        menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+      }
+    };
+    document.addEventListener('click', (event) => {
+      const id = event.target.id;
+      const superhero = event.target.getAttribute('data-superhero');
+      if (id === 'hamburger-icon') {
+        // toggle when clicked on icon
+        toggleHamburgerMenu();
+      } else if (id === 'hamburger-menu' || superhero) {
+        // Restrict close inside menu
+        toggleHamburgerMenu('flex');
+      } else {
+        // close when clicked anywhere else on window
+        toggleHamburgerMenu('none');
+      }
+    });
+//})();
---
+++ project1.js
@@ -0,0 +1,117 @@
+const project1 = `
+<section id="project1" class="project1">
+    <h1>Tensor Flow Body pix<h1>
+    <h2>
+        BodyPix: Person Segmentation in the Browser with TensorFlow.js using React
+    </h2>
+    <div>
+        <p>BodyPix, an open-source machine learning model which allows for person and body-part segmentation in the browser with TensorFlow.js. With default settings, it estimates and renders person and body-part segmentation at 25 fps</p>
+    </div>
+    <div>
+        <img src="./assets/bpix.gif" width="320" height="240"/>
+    </div>
+    <div class="pdT20">
+        <p> What exactly is person segmentation? In computer vision, image segmentation refers to the technique of grouping pixels in an image into semantic areas typically to locate objects and boundaries. The BodyPix model is trained to do this for a person and twenty-four body parts (parts such as the left hand, front right lower leg, or back torso). In other words, BodyPix can classify the pixels of an image into two categories: 1) pixels that represent a person and 2) pixels that represent background. It can further classify pixels representing a person into any one of twenty-four body parts.</p>
+    <div>
+    <div class="pdT20">
+        <p>What can be done with person segmentation? This technology can be a tool for a wide range of applications from augmented reality to photography editing to artistic effects on images or videos. It’s up to you! With PoseNet (the first model ever that allowed for body estimation (what a Kinect does) in the browser using simple webcams), people came up with a wide variety of use cases for the technology. The same kind of creative experimentation happens with BodyPix.</p>
+    </div>
+    <div class="pdT20">
+        <h2>Body Pix with React JS</h2>
+    </div>
+    <div class="pdT20">
+        <p>The library can be installed with: npm install @tensorflow-models/body-pix and then imported using npm:</p>
+    </div>
+    <pre class="language-none pdT20">
+        <code>
+            "@tensorflow-models/body-pix": "2.2.0",
+            "@tensorflow/tfjs": "3.13.0",
+            "@tensorflow/tfjs-backend-webgl": "3.13.0",
+            "@tensorflow/tfjs-converter": "3.13.0",
+            "@tensorflow/tfjs-core": "3.13.0"
+        </code>
+    </pre>
+    <div class="pdT20">
+        <p>Load body pix in component and use segmentation algorithm</p>
+        <pre class="language-none pdT20">
+            <code>
+                const net = await bodypix.load({
+                    architecture: 'ResNet50',
+                    outputStride: 32,
+                    quantBytes: 4
+                })
+            </code>
+            <code>
+                const segmentation = await net.segmentPerson(canvas, {
+                    internalResolution: 'full',
+                    segmentationThreshold: 0.8,
+                    scoreThreshold: 0.9
+                })
+            </code>
+            <code>
+            //Segmenting the pixels with 0(background) and 1(body)
+
+            segmentation.data.forEach((segment, i) => {
+                if (segment === 1) {
+                  newImgData[i * 4] = imgData[i * 4]
+                  newImgData[i * 4 + 1] = imgData[i * 4 + 1]
+                  newImgData[i * 4 + 2] = imgData[i * 4 + 2]
+                  newImgData[i * 4 + 3] = imgData[i * 4 + 3]
+                }
+              })
+            </code>
+        </pre>
+    </div>
+    <div class="pdT20"><h2>Person Segmentation Output</h2></div>
+    <p>An example output person segmentation looks like:</p>
+    <pre class="language-none">
+        <code>
+        {
+            width: 640,
+            height: 480,
+            data: Uint8Array(307200) [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, …]
+          }
+          // the data array contains 307200 values, one for each pixel of the 640x480 image that was passed to the function.
+        </code>
+    </pre>
+    <div class="flex pdT20">
+        <div>
+            <div>Before</div>
+            <div><img src="./assets/before.jpg" height="430px"/></div>
+        </div>
+        <div>
+            <div>After</div>
+            <div><img src="./assets/after.PNG" height="430px"/></div>
+        </div>
+    </div>
+    <div class="pdT20">
+        <h1>Body Part Segmentation</h1> 
+        <img class="pdT10" src="./assets/bp.png" width="500" height="460"/>
+    </div>
+    <div class="pdT10">
+        <p>We call the API method estimatePartSegmentation to perform body part segmentation on an image or video; this short code block shows how to use it:</p>
+        <pre class="language-none">
+            <code>
+                const imageElement = document.getElementById('image');
+        
+                // load the BodyPix model from a checkpoint
+                const net = await bodyPix.load();
+        
+                // arguments for estimating body part segmentation.
+                const outputStride = 16;
+                const segmentationThreshold = 0.5;
+        
+                // load the person segmentation model from a checkpoint
+                const net = await bodyPix.load();
+        
+                const partSegmentation = await net.estimatePartSegmentation(imageElement, outputStride, segmentationThreshold);
+            </code>
+        </pre>
+    </div>
+    <div class="pdT20">
+        <h1>How does it work internally</h1>
+        <p class="pdT10">At the core of BodyPix is an algorithm that performs body segmentation — or, in other words, performs a binary decision for each pixel of an input image to estimate whether that pixel belongs to a person or not</p>
+        <img class="pdT10" src="./assets/10.png" width="80%"/>
+    </div>
+</div>
+</section>`
